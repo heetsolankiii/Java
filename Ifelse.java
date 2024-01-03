07cdff10e2a43290ebe78b39af82a0579e024d97
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-01-03 16:05:30 +0530

    Create Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Ifelse {
+    public static void main(String[] args) {
+        int x=10;
+        int y=1;
+        if (x+y>20) {
+            System.out.println("x+y is greater than 20.");
+        }
+        else{
+            System.out.println("x+y is smaller than 20.");
+        }
+    }
+}
