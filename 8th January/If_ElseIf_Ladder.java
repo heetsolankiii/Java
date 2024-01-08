c4a011025a2793166556c52f17f7445b6c387065
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-08 21:24:29 +0530

    Create If_ElseIf_Ladder.java

---
+++ 8th January/If_ElseIf_Ladder.java
@@ -0,0 +1,20 @@
+public class If_ElseIf_Ladder {
+    public static void main(String[] args) {
+        int number = 69;
+        if (number == 0) 
+        {
+            System.out.println("The number is equal to ZERO.");
+        } 
+        else 
+        {
+           if (number > 0) 
+           {
+                System.out.println("The number is POSITIVE.");
+           } 
+           else 
+           {
+                System.out.println("The number is NEGATIVE.");
+           } 
+        }
+    }    
+}
