aa7c1f5943fe73c5fbba7f0d84e4b94d57e87a38
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-13 10:21:45 +0530

    Added Comments

--- 12th January/Pascal.java
+++ 12th January/Pascal.java
@@ -3,19 +3,19 @@ public class Pascal
     public static void main(String[] args)
     {
       int outerValue, innerValue, spaces, num;
-      for(outerValue=0; outerValue<5; outerValue++)
+      for(outerValue=0; outerValue<5; outerValue++)     //Used to print the outer values
         {
           for(spaces=5; spaces>outerValue; spaces--)
             {
-              System.out.print(" ");
+              System.out.print(" ");        //Used to print spaces before outer values
             }
           num = 1;
           for(innerValue=0; innerValue<=outerValue; innerValue++)
             {
-              System.out.print(num + " ");
-              num = (num * (outerValue-innerValue)/(innerValue+1));
+              System.out.print(num + " ");      //Used to print inner values
+              num = (num * (outerValue-innerValue)/(innerValue+1));     //Used to increment inner values
             }
-          System.out.print("\n");
+          System.out.print("\n");   //Used for next line
         }
     }
 }
