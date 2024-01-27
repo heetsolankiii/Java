be44e3553b7e78334a05fcca54f1d88ff1f0898b
heetsolankiii <heetsolanki2006@gmail.com>
2024-01-27 13:00:39 +0530

    Create Classes.java

---
+++ 25th January/Classes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Classes {
+    static void Add(int a, int b) {
+        int add = a + b;
+        System.out.println(add);
+    }
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter value for a: ");
+        int a = userInput.nextInt();
+        System.out.print("Enter value for b: ");
+        int b = userInput.nextInt();
+        Add(a, b);
+    }
+}
