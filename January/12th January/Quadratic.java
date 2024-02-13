3e814037159f6de06c47b4af08c988c19b85d693
heetsolankiii <heetsolanki2006@gmail.com>
2024-02-13 16:59:08 +0530

    Update Quadratic.java

--- January/12th January/Quadratic.java
+++ January/12th January/Quadratic.java
@@ -1,11 +1,20 @@
+import java.util.*;
+
 public class Quadratic
 {
     public static void main(String[] args) 
     {
-        int a = 1, b = 5, c = 2;
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Enter value for a: ");
+        int a = userInput.nextInt();
+        System.out.print("Enter value for b: ");
+        int b = userInput.nextInt();
+        System.out.print("Enter value for c: ");
+        int c = userInput.nextInt();
         double roots1,roots2;
         roots1 = (-b + Math.sqrt((b*b) - 4*a*c))/2*a;
         roots2 = (-b - Math.sqrt((b*b) - 4*a*c))/2*a;
         System.out.println("Roots are: " + roots1 + "&" + roots2);
+        userInput.close();
     }
 }
