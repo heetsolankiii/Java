88868b0788fae22c8fe18ff3b2b661d53d08bf9d
heetsolankiii <heetsolanki2006@gmail.com>
2024-03-02 11:01:03 +0530

    Create TryCatchFour.java

---
+++ March/TryCatchFour.java
@@ -0,0 +1,11 @@
+public class TryCatchFour {
+    public static void main(String[] args) {
+        try {
+            int arr[] = {1, 3, 5, 7};
+            System.out.println(arr[10]);
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
+    }
+}
